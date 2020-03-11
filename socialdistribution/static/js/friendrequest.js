d86bb8e084b68f4756e0455b34e5e739e8b5f91d
XiaoleZ <xiaole2@ualberta.ca>
2020-03-11 00:11:02 -0600

    use jquery to get the number of friend request

---
+++ socialdistribution/static/js/friendrequest.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // TODO: get the django friend request object,
+    // If we have the friend/author api to know how 
+    // many friend requests the author has
+    $(".badge").text(7);
+});
