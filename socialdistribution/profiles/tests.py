8d63a58709d876c51ba388f4267784ec2e33219c
cmput274 <gregdraghici@gmail.com>
2020-03-12 15:37:46 -0400

    Fixed url in test, as urls changed

--- socialdistribution/profiles/tests.py
+++ socialdistribution/profiles/tests.py
@@ -13,14 +13,21 @@ class Profiles_Test(TestCase):
         User = get_user_model()
         print("In test")
         client = Client()
+        print("MAde user")
 
         # Logging in, and retreiving stream (can only do when logged in)
         login_action = client.login( email='to@to.com', password='wrongaccount')
-        login_stream = client.get('/stream/')
+        login_stream = client.get('/accounts/password_change/')
         self.assertTrue(login_stream.status_code == 200)
 
         # Logout, and retreive stream (returns HTTP 302 to redirect to /accounts/login)
         logout_action = client.get('/accounts/logout/')
-        logout_stream = client.get('/stream/')
+        logout_stream = client.get('/accounts/password_change/')
         self.assertTrue(logout_stream.status_code == 302)
         self.assertTrue("/accounts/login" in logout_stream.url)
+
+    def test_account_creation(self):
+        client = Client()
+        # User = get_user_model()
+        # print(User.objects.get(email = "to@to.com"))
+        # print("AFTER")
