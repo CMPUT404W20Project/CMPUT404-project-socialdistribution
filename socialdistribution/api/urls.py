b86e9ce18c879372ddc0d177a12fd417d6c5f5bd
cmput274 <gregdraghici@gmail.com>
2020-03-26 18:28:01 -0400

    Changed decorator to fix auth/csrf issue

--- socialdistribution/api/urls.py
+++ socialdistribution/api/urls.py
@@ -1,50 +1,54 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 from . import views
+
+# Decorated url instead of view as csrf_exempt
+# https://stackoverflow.com/a/60252189
 
 urlpatterns = [
     path(
         "posts",
-        views.posts,
+        csrf_exempt(views.posts),
         name="api_get_all_public_posts"),
     path(
         "posts/<uuid:post_id>",
-        views.single_post,
+        csrf_exempt(views.single_post),
         name="api_get_single_post"),
     path(
         "posts/<uuid:post_id>/comments",
-        views.post_comments,
+        csrf_exempt(views.post_comments),
         name="api_post_comments"),
     path(
         "author/posts",
-        views.author_posts,
+        csrf_exempt(views.author_posts),
         name="api_get_author_posts"),
     path(
         "author/<uuid:author_id>",
-        views.author_profile,
+        csrf_exempt(views.author_profile),
         name="api_get_author_profile"),
     path(
         "author/<uuid:author_id>/posts",
-        views.specific_author_posts,
+        csrf_exempt(views.specific_author_posts),
         name="api_get_specific_author_posts"),
     path(
         "author/<uuid:author_uuid>/friends",
-        views.author_friends,
+        csrf_exempt(views.author_friends),
         name="api_get_author_friends"),
     path(
         "author/<uuid:author_uuid>/friends/<path:author_friend_url>",
-        views.author_friends_with_author,
+        csrf_exempt(views.author_friends_with_author),
         name="api_get_author_friends_with_author"),
     path(
         "friendrequest",
-        views.friend_request,
+        csrf_exempt(views.friend_request),
         name="api_friend_request"),
     path(
         "whoami",
-        views.who_am_i,
+        csrf_exempt(views.who_am_i),
         name="api_who_am_i"),
     path(
         "cansee/<uuid:author_id>/<uuid:post_id>",
-        views.can_see,
+        csrf_exempt(views.can_see),
         name="api_can_see"),
 ]
